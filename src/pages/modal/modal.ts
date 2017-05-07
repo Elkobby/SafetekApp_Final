958961152a3ccddade5029bf72eead0720c20247
maaddae <urntipz@gmail.com>
2017-05-07 15:18:15 -0800

    Modified modalPage to add a group at a meetpoint

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
 import {UserActionPage} from '../user-action/user-action'; 
 
 /*
@@ -13,8 +15,22 @@ import {UserActionPage} from '../user-action/user-action';
   templateUrl: 'modal.html'
 })
 export class ModalPage {
+  public data: any;
+  public items: any = [];
+  public post: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public http: Http) {
+    this.data = {};
+    this.data.gname = "";
+    this.data.mid = this.navParams.get("mId");
+    this.data.sid = this.navParams.get("sId");
+    this.data.hid = this.navParams.get("hId");
+    this.data.gid = this.navParams.get("gId");
+    //console.log('sId: '+ this.data.sid);
+    //console.log('hId: '+ this.data.hid);
+    //console.log('gId: '+ this.data.gid);
+    //console.log('mId: '+ this.data.mid);
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ModalPage');
@@ -22,7 +38,31 @@ export class ModalPage {
    dismiss() {
     this.viewCtrl.dismiss()}
 
-  pushPage() {
-    this.navCtrl.setRoot(UserActionPage); 
+
+  createGroup(){
+    let gname = this.data.gname;
+
+    let body     : string = "key=create&gname=" + gname +"&sId="+ this.data.sid + "&mId="+ this.data.mid,
+        type     : string = "application/x-www-form-urlencoded; charset=UTF-8",
+        headers   : any    = new Headers({ 'Content-Type': type}),
+        options   : any    = new RequestOptions({ headers: headers }),
+        url     : any    = "http://nibbles/webfiles/safetekApp/retreive-groups.php";
+
+    this.http.post(url, body, options)
+        .map(res => res.json())
+        .subscribe(data =>
+        {
+          this.post = JSON.stringify(data);
+          this.initializeItems();
+          this.data.gid = this.items.replace(/\"/g, "");
+          console.log('gId: '+this.items)
+          this.navCtrl.push(UserActionPage, {sId: this.data.sid, hId: this.data.hid, gId: this.data.gid, mId: this.data.mid });
+        });
+    
   }
+
+  initializeItems() {
+    this.items = this.post;
+  }
+
 }
