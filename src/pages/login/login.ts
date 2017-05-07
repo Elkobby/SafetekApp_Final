3489ce4ecf70dfb389d2c423a707a7b82e3c5e79
maaddae <urntipz@gmail.com>
2017-05-07 14:34:21 -0800

    Updated the login.ts and modified the login.html

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {NavController, AlertController, LoadingController} from 'ionic-angular';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
 import {UserpagePage} from '../userpage/userpage';
-
+import {SignupPage} from "../signup/signup";
 /*
   Generated class for the Login page.
 
@@ -13,15 +15,48 @@ import {UserpagePage} from '../userpage/userpage';
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  public data : any;
+  public post: any;
+  public firstname: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController,private http : Http, private alert : AlertController,
+              private loading : LoadingController) {
+    this.data = {};
+    this.data.reference = "";
+    this.data.index = "";
+    this.data.password = "";
+  }
+
+  login(){
+
+    let     body     : string = "reference=" + this.data.reference +"&index="+ this.data.index +"&password="+ this.data.password,
+            type     : string = "application/x-www-form-urlencoded; charset=UTF-8",
+            headers   : any    = new Headers({ 'Content-Type': type}),
+            options   : any    = new RequestOptions({ headers: headers }),
+            url     : any    = "http://nibbles/webfiles/safetekApp/signin.php";
+
+        this.http.post(url, body, options)
+        .map(res => res.json())
+        .subscribe(data=>{
+              this.post = data;
+              console.log("user: "+JSON.stringify(this.post));
+              let firstname = JSON.stringify(this.post.firstname);
+              let loader = this.loading.create({content: "Checking, Please wait...", duration: 500});
+              loader.present();
+              this.navCtrl.push(UserpagePage, { id: this.post.sId, name: firstname});
+              },
+              error=>{ let alert = this.alert.create({title: 'Warning', subTitle: 'Reference | Index | Password combination is Incorrect', buttons:["ok"]});
+              alert.present();
+
+        });
+  }
+
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+        console.log('ionViewDidLoad LoginPage');
   }
-
-  pushPage(){
-    this.navCtrl.setRoot(UserpagePage);
-  }
+  pushPage() {
+        this.navCtrl.push(SignupPage);
+    }
 
 }
