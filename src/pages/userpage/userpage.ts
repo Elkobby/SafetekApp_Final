f1a43bafb0220f8e5546d7064196edc919b62383
maaddae <urntipz@gmail.com>
2017-05-07 14:47:27 -0800

    Modified the userpage to output/display the hostels and the ability to live-search

--- src/pages/userpage/userpage.ts
+++ src/pages/userpage/userpage.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams,  AlertController, LoadingController } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
 import {MeetpointsPage} from '../../pages/meetpoints/meetpoints'; 
-// import {HostelService} from '../../providers/hostel-service';
-import {HostelSearch} from '../../providers/hostel-search';
-
-import 'rxjs/add/operator/map'; 
-
 
 /*
   Generated class for the Userpage page.
+
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
@@ -17,41 +15,71 @@ import 'rxjs/add/operator/map';
   templateUrl: 'userpage.html'
 })
 export class UserpagePage {
-  
-  hostelName: string = '';
-  hostels: any; 
-  
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public hostelSearch: HostelSearch)  {
-  this.loadHostels();
-  
+  private posts: any; 
+  private items: any;
+  public firstname: any;
+  public sId: any;
+
+  constructor(private http: Http, private loadingCtrl: LoadingController, public navCtrl: NavController, private navParams: NavParams) {
+
+   if(this.navParams.get("name")){
+     this.firstname = this.navParams.get('name').replace(/\"/g, "");
+     this.sId = this.navParams.get('id');
+     console.log(this.firstname);
+     console.log(this.sId);
+    }
+
+    // Show the loading message
+    let loadingPopup = this.loadingCtrl.create({
+      content: 'Loading posts...'
+    });
+
+    this.http.get('http://nibbles/webfiles/safetekApp/retreive-hostels.php')
+        .map(res => res.json())
+        .subscribe(data => {
+      this.posts = data;
+      this.initializeItems();
+
+      // Hide the loading message
+      loadingPopup.dismiss();
+    });
   }
+
 
   ionViewDidLoad() {
-    
-    // console.log('ionViewDidLoad UserpagePage');
-   this.hostelSearch.getHostels();
-  //  this.setSearchedHostels(); 
- 
-}
-  // Function for displaying hostels
-  loadHostels(){
-    this.hostelSearch.getHostels()
-    .then(data =>{
-      this.hostels = data; 
-    }); 
+    console.log('ionViewDidLoad UserpagePage');
+   }
+
+  viewEntry({record: item})
+  {
+    this.navCtrl.push(MeetpointsPage, {record: item, sId: this.sId});
   }
 
-  //Functions for naviagting between pages
-  pushPage() {
-    this.navCtrl.push(MeetpointsPage);
+
+  initializeItems() {
+    this.items = this.posts;
+    //console.log(this.items);
   }
 
-  //Implement search hostels
-  // setSearchedHostels(){
-  //   this.hostels = this.hostelSearch.searchHostels(this.hostelName); 
-  // }
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
 
- 
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
 
 }
+
+
+
+
